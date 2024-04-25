cfbc279522f4e7ee1454ff100e78bf7a137c2ae9
ben-horst <ben.horst@flyzipline.com>
2024-04-25 15:12:31 -0700

    Working tester

--- run_short_circuit_test.py
+++ run_short_circuit_test.py
@@ -5,12 +5,13 @@ import numpy as np
 
 #-------------user input------------------
 datarate = 10000                     #datarate in Hz of logging
-short_time = 2                       #duration of short circuit, seconds
+short_time = 1                       #duration of short circuit, seconds
 pyro_current_threshold = 20.0        #current in A that trigger pyro to blow, when evaluated after pyro_fire_time
+test_name = 'LeadAcidTest'         #name included in plot title and logfile
 #-------------user input------------------
 
 
-tester = ShortCircuitTester(datarate, short_time, pyro_current_threshold)
+tester = ShortCircuitTester(datarate, short_time, pyro_current_threshold, test_name)
 tester.run_test()
 tester.save_data()
 tester.plot()
--- short_circuit_tester.py
+++ short_circuit_tester.py
@@ -6,11 +6,13 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 class ShortCircuitTester:
-    def __init__(self, datarate, short_time, pyro_current_threshold):
+    def __init__(self, datarate, short_time, pyro_current_threshold, test_name):
+        self.test_name = test_name
         self.cDAQ_name = 'cDAQ9184-186A96E'
         self.datarate = datarate                     #datarate in Hz of logging
         self.samples_before_buffer = 1000        #number of samples to be captured in buffer before grabbing from device
-        self.pre_log_time = 1.0                 #duration of pre-short log time, seconds
+        self.pre_log_time = 0.75                 #duration of pre-short log time, seconds
+        self.delay_before_SCR = 0.25            #time between closing contactor and activating SCR
         self.short_time = short_time                  #duration of short circuit, seconds
         self.scr_pulse_time = 0.1                #duration of SCR pulse, seconds
         self.pyro_pulse_time = 0.2                #duration of pyro pulse, seconds
@@ -23,12 +25,11 @@ class ShortCircuitTester:
         self.light_task = nidaqmx.Task('light')
         self.analog_task = nidaqmx.Task('analog_inputs')
         self.volt_scale = 10
-        #self.shunt_resistance = 0.0001  #ohms
-        self.shunt_resistance = 0.001  #ohms
+        self.shunt_resistance = 0.0001  #ohms
         self.data_volt = []
         self.data_current = []
         self.data_time = []
-        self.reset_cDAQ()
+        #self.reset_cDAQ()
         self.initialize_outputs()
         self.initialize_inputs()
 
@@ -85,7 +86,7 @@ class ShortCircuitTester:
     def check_to_fire_pyro(self):
         last_currents = self.data_current[-100:]     #last 100 current measurements
         avg_current = sum(last_currents) / len(last_currents)
-        if avg_current > self.pyro_current_threshold:
+        if abs(avg_current) > self.pyro_current_threshold:
             self.fire_pyro()
             return f'pyro fired at current of {avg_current} A'
         else:
@@ -93,7 +94,7 @@ class ShortCircuitTester:
 
     def initialize_inputs(self):
         self.analog_task.ai_channels.add_ai_voltage_chan(f"{self.cDAQ_name}Mod1/ai0")
-        self.analog_task.ai_channels.add_ai_voltage_chan(f"{self.cDAQ_name}Mod1/ai2")
+        self.analog_task.ai_channels.add_ai_voltage_chan(f"{self.cDAQ_name}Mod1/ai1")
         self.analog_task.timing.cfg_samp_clk_timing(self.datarate, sample_mode=AcquisitionType.CONTINUOUS)
         self.analog_task.register_every_n_samples_acquired_into_buffer_event(self.samples_before_buffer, self.callback)
     
@@ -112,45 +113,59 @@ class ShortCircuitTester:
     def stop_measuring(self):
         self.append_data(READ_ALL_AVAILABLE)
         self.analog_task.stop()
-        time.sleep(1)
-        #self.data_volt = self.data_volt[0:len(self.data_current)]      #DELETE THIS IF IT WORKS
         self.data_time = np.arange(len(self.data_volt)) / self.datarate
+        time_offset = self.pre_log_time + self.delay_before_SCR
+        self.data_time = np.subtract(self.data_time, time_offset)   #shift time vector so t=0 is start of short
 
     def save_data(self):
-        data_array = np.asarray([self.data_time, self.data_volt, self.data_current])    #the individual arrays may need to be turned vertical
-        filename = "short_test-" + datetime.now().strftime("%m/%d/%Y-%H:%M:%S") + '.csv'
+        data_array = np.asarray([self.data_time, self.data_volt, self.data_current]).transpose()
+        filename = 'logs/' + self.test_name + '-' + datetime.now().strftime("%m_%d_%Y-%H_%M_%S") + '.csv'
         header = 'Time (s),Battery Voltage (V),Current (A)'
-        np.savetxt(filename, data_array, delimiter=',', header=header)
+        np.savetxt(filename, data_array, delimiter=',', header=header, fmt='%0.5f')
     
     def plot(self):
         fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
         ax1.plot(self.data_time, self.data_volt)
         ax2.plot(self.data_time, self.data_current)
-        ax2.xlabel("Time (s)")
-        ax1.ylabel("Battery Voltage (V)")
-        ax2.ylabel("Current (A)")
+        fig.suptitle(f"Short Circuit - {self.test_name}")
+        plt.xlabel("Time (s)")
+        ax1.set(ylabel='Battery Voltage (V)')
+        ax2.set(ylabel='Current (A)')
         plt.show()
 
+    def start_countdown(self):
+        input('press enter to start countdown')
+        print('-----short circuit test initiated - press ctrl-c to abort------')
+        for i in range(5, 0, -1):
+            print(i)
+            time.sleep(1)
+
     def run_test(self):
+        self.start_countdown()
         self.light_on()
         self.start_measuring()
         print(f'measurement started at {self.datarate} Hz')
-        print(f'wait for {self.pre_log_time}')
+        print(f'wait for {self.pre_log_time} seconds (pre-logging)')
         time.sleep(self.pre_log_time)
         self.close_contactor()
         print('contactor closed')
-        time.sleep(0.5)
+        print(f'wait for {self.delay_before_SCR} seconds (delay after closing contactor)')
+        time.sleep(self.delay_before_SCR)
         self.pulse_scr()
         print('SCR activated')
+        print(f'shorting for {self.short_time} seconds')
         time.sleep(self.short_time)
-        self.close_contactor()
+        self.open_contactor()
         print('contactor opened')
+        print(f'wait for {self.pyro_evaluation_time} seconds (delay for evaluating pyro)')
         time.sleep(self.pyro_evaluation_time)
         pyro_result = self.check_to_fire_pyro()
         print(pyro_result)
-        print(f'wait for {self.post_log_time}')
+        print(f'wait for {self.post_log_time} seconds (post-logging)')
         time.sleep(self.post_log_time)
         self.stop_measuring()
         print(f'measurement stopped - {len(self.data_time)} data points recorded')
-        self.disable_outputs()
+        max_current = max(self.data_current)
+        print(f'test completed - max current = {max_current} A')
         self.light_off()
+        self.disable_outputs()
